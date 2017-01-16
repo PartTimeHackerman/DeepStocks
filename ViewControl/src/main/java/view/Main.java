bb925ad66e3bde654769cb9c1ee0c1721488b00e
WorstCoder <kuriozalnie@gmail.com>
2017-01-16 17:52:00 +0100

    Initial frontend setup

--- ViewControl/src/main/java/view/Main.java
+++ ViewControl/src/main/java/view/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		
-		stocksLoader = new StocksLoader();
+		//stocksLoader = new StocksLoader();
 		
 		launch();
 	}
