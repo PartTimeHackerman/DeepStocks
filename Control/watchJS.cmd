07eda0f3bde23547917ec8efb3f1282d841b841b
WorstCoder <kuriozalnie@gmail.com>
2017-04-22 03:35:01 +0200

    Modified webpack watch and webpack dev server script to batch

--- Control/watchJS.cmd
+++ Control/watchJS.cmd
@@ -2,9 +2,11 @@
 REM This is a simple helper script used to start the webpack watch process
 REM so that javascript files can be automatically reloaded when they are saved.
 REM The version of node must match what's in gradle.
+REM %~dp0%NODE_LOC% %~dp0%WEBPACK_LOC% --watch & %~dp0%NODE_LOC% & ...
 
-SET NODE_VERSION=5.8.0
+SET NODE_VERSION=6.9.4
 SET NODE_LOC=.gradle\nodejs\node-v%NODE_VERSION%-windows-x64\bin\node
 SET WEBPACK_LOC=node_modules\webpack\bin\webpack.js
+SET WEBPACK_DEV_LOC=node_modules\webpack-dev-server\bin\webpack-dev-server.js
 
-%~dp0%NODE_LOC% %~dp0%WEBPACK_LOC% --watch --display-error-details
+%~dp0%NODE_LOC% %~dp0%WEBPACK_DEV_LOC% --progress --colors --inline --port 8081 & %~dp0%NODE_LOC% %~dp0%WEBPACK_LOC% --watch & %~dp0%NODE_LOC%
