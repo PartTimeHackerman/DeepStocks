d549f21570ffb5dfb3aeef570fee7523128d9cc1
WorstCoder <kuriozalnie@gmail.com>
2017-05-19 12:43:39 +0200

    Refactoring

--- Model/src/main/java/model/connection/packetHandler/TicksHistoryHandler.java
+++ Model/src/main/java/model/connection/packetHandler/TicksHistoryHandler.java
@@ -1,19 +1,18 @@
 package model.connection.packetHandler;
 
-import io.reactivex.subscribers.DisposableSubscriber;
 import lombok.Data;
+import model.connection.validation.RequestValidator;
 import model.connection.SimpleStream;
 import model.data.Candle;
 import model.binaryAPI.commands.ticks_history.TicksHistoryReceive;
 import model.connection.Packet;
 import model.data.Stock;
 import model.data.StockRepo;
-import model.utils.MainLogger;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -21,18 +20,20 @@ import java.util.function.Predicate;
 public class TicksHistoryHandler extends SimpleStream<TicksHistoryHandler.StockCandlesWrapper> implements PacketHandler {
 	
 	private final StockRepo stockRepo;
-	
+	private final RequestValidator requestValidator;
 	//private final Flowable<Candle> flowable;
 	
 	@Autowired
-	public TicksHistoryHandler(StockRepo stockRepo) {
+	public TicksHistoryHandler(StockRepo stockRepo, RequestValidator requestValidator) {
 		this.stockRepo = stockRepo;
+		this.requestValidator = requestValidator;
 	}
 	
 	@Override
 	public void handle(Packet packet) {
-		List<Candle> candles = getCandles(packet);
 		Stock stock = getStock((Integer) packet.getOptional());
+		List<Candle> candles = getCandles(packet);
+		validateResponse(stock, candles);
 		
 		StockCandlesWrapper stockCandlesWrapper = new StockCandlesWrapper(stock, candles);
 		submit(stockCandlesWrapper);
@@ -55,13 +56,20 @@ public class TicksHistoryHandler extends SimpleStream<TicksHistoryHandler.StockC
 		return ticksReceive.getCandles();
 	}
 	
+	private void validateResponse(Stock stock, Collection<Candle> candles) {
+		if (candles.isEmpty())
+			return;
+		Long epochFrom = candles.stream().map(Candle::getEpoch).min(Comparator.comparingLong(v -> v)).get();
+		Long epochTo = candles.stream().map(Candle::getEpoch).max(Comparator.comparingLong(v -> v)).get();
+	}
+	
 	@Data
-	public static final class StockCandlesWrapper{
+	public static final class StockCandlesWrapper {
 		
 		private final Stock stock;
 		private final List<Candle> candles;
 		
-		public StockCandlesWrapper(Stock stock, List<Candle> candles){
+		public StockCandlesWrapper(Stock stock, List<Candle> candles) {
 			this.stock = stock;
 			this.candles = candles;
 		}
--- Model/src/main/java/model/connection/websocket/WebsocketClient.java
+++ Model/src/main/java/model/connection/websocket/WebsocketClient.java
@@ -3,114 +3,109 @@ package model.connection.websocket;
 import model.utils.MainLogger;
 import org.glassfish.tyrus.client.ClientManager;
 import org.glassfish.tyrus.client.ClientProperties;
+import scraper.Proxy;
 
 import javax.websocket.*;
 import java.io.IOException;
 import java.net.URI;
+import java.util.function.Consumer;
 
 @ClientEndpoint
 public class WebsocketClient {
 	
 	private Session userSession = null;
-	
-	private MessageHandler messageHandler;
-	
+	private Consumer<String> messageHandler;
 	private ClientManager client;
-	private String proxy = "";
+	
+	private URI uri;
+	private Proxy proxy;
+	
 	
 	public WebsocketClient(URI uri) throws IOException, DeploymentException {
-		client = ClientManager.createClient();
-		setUpClient(client, uri);
+		this(uri, null);
 	}
 	
-	public WebsocketClient(URI uri, String ip, String port) throws IOException, DeploymentException {
-		MainLogger.log().debug("Creating new websocket ip: {} port: {}", ip, port);
+	public WebsocketClient(URI uri, Proxy proxy) throws IOException, DeploymentException {
+		MainLogger.log(this).debug("Creating new websocket");
+		Long time = setUpWebsocket(uri, proxy);
+		MainLogger.log(this).debug("Websocket api created in {} millis.", time);
+	}
+	
+	private Long setUpWebsocket(URI uri, Proxy proxy) throws IOException, DeploymentException {
+		this.uri = uri;
+		this.proxy = proxy;
+		this.client = ClientManager.createClient();
 		Long time = System.currentTimeMillis();
-		
-		client = ClientManager.createClient();
-		setUpClient(client, uri, ip, port);
-		
-		MainLogger.log().debug("Websocket api created in {} millis.", System.currentTimeMillis() - time);
+		setUpClient();
+		if (proxy != null)
+			setUpClientProxy(client, proxy);
+		client.connectToServer(this, uri);
+		return System.currentTimeMillis() - time;
 	}
 	
-	private void setUpClient(ClientManager client, URI uri) throws IOException, DeploymentException {
+	private void setUpClient() throws IOException, DeploymentException {
 		client.setDefaultMaxTextMessageBufferSize(3276800);
 		client.setDefaultMaxBinaryMessageBufferSize(3276800);
 		client.setDefaultMaxSessionIdleTimeout(0);
-		client.connectToServer(this, uri);
 	}
 	
-	private void setUpClient(ClientManager client, URI uri, String ip, String port) throws IOException, DeploymentException {
-		proxy = "http://" + ip + ":" + port;
-		client.getProperties().put(ClientProperties.PROXY_URI, proxy);
-		setUpClient(client, uri);
+	private void setUpClientProxy(ClientManager client, Proxy proxy) throws IOException, DeploymentException {
+		String proxyAddress = "http://" + proxy.getIp() + ":" + proxy.getPort();
+		client.getProperties().put(ClientProperties.PROXY_URI, proxyAddress);
 	}
 	
-	/**
-	 * Callback hook for Connection open events.
-	 *
-	 * @param userSession the userSession which is opened.
-	 */
-	@OnOpen
-	public void onOpen(Session userSession) {
-		MainLogger.log().info("Opening websocket, {}", userSession);
-		this.userSession = userSession;
-	}
-	
-	/**
-	 * Callback hook for Connection close events.
-	 *
-	 * @param userSession the userSession which is getting closed.
-	 * @param reason      the reason for connection close
-	 */
-	@OnClose
-	public void onClose(Session userSession, CloseReason reason) {
-		MainLogger.log().warn("Closing websocket, {}, {}", userSession, reason);
-		this.userSession = null;
-	}
-	
-	/**
-	 * Callback hook for Message Events. This method will be invoked when a client send a message.
-	 *
-	 * @param message The text message
-	 */
-	@OnMessage
-	public void onMessage(String message) {
-		if (this.messageHandler != null) {
-			this.messageHandler.handleMessage(message);
-		}
-	}
-	
-	/**
-	 * register message handler
-	 *
-	 * @param msgHandler
-	 */
-	public void addMessageHandler(MessageHandler msgHandler) {
-		this.messageHandler = msgHandler;
-	}
-	
-	/**
-	 * Send a message.
-	 *
-	 * @param message
-	 */
 	public void sendMessage(String message) {
 		try {
 			this.userSession.getBasicRemote().sendText(message);
 		} catch (IOException e) {
-			e.printStackTrace();
+			MainLogger.log(this).error(e);
 		}
+	}
+	
+	@OnMessage
+	public void onMessage(String message) {
+		if (this.messageHandler != null) {
+			this.messageHandler.accept(message);
+		}
+	}
+	
+	@OnOpen
+	public void onOpen(Session userSession) {
+		MainLogger.log(this).info("Opening websocket, {}", client);
+		this.userSession = userSession;
+	}
+	
+	@OnClose
+	public void onClose(Session userSession, CloseReason reason) {
+		MainLogger.log(this).warn("Closing websocket, {}, {}", client, reason);
+		this.userSession = null;
+	}
+	
+	public void addMessageHandler(Consumer<String> msgHandler) {
+		this.messageHandler = msgHandler;
 	}
 	
 	public ClientManager getClient() {
 		return client;
 	}
 	
-	/**
-	 * Message handler.
-	 */
-	public static interface MessageHandler {
-		public void handleMessage(String message);
+	public Boolean isConnected() {
+		return userSession != null;
+	}
+	
+	public Boolean reconnect() {
+		MainLogger.log(this).info("Reconnecting websocket {}", client);
+		try {
+			setUpWebsocket(this.uri, this.proxy);
+			MainLogger.log(this).info("Reconnected websocket {}", client);
+			return true;
+		} catch (IOException | DeploymentException e) {
+			return false;
+		}
+	}
+	
+	
+	public Proxy getProxy() {
+		return proxy;
 	}
 }
