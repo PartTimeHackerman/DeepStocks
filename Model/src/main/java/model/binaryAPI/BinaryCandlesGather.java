053d55e8f4565be2be6ca34092a43015dc07bf45
WorstCoder <kuriozalnie@gmail.com>
2017-07-12 21:40:48 +0200

    Implemented getLatestCandle() and decomposed getPacket()

--- Model/src/main/java/model/binaryAPI/BinaryCandlesGather.java
+++ Model/src/main/java/model/binaryAPI/BinaryCandlesGather.java
@@ -3,32 +3,45 @@ package model.binaryAPI;
 import model.binaryAPI.commands.ticks_history.TicksHistorySend;
 import model.connection.PacketSender;
 import model.connection.Packet;
-import model.connection.validation.RequestValidator;
+import model.connection.packetHandler.TicksHistoryHandler;
+import model.data.Candle;
 import model.data.Stock;
 import model.data.StockProvider;
-import model.connection.packetHandler.TicksHistoryHandler;
 import model.data.Symbol;
 import model.utils.EpochUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class BinaryCandlesGather {
 	
 	private final PacketSender packetSender;
+	private final TicksHistoryHandler ticksHistoryHandler;
 	private Integer count = 1000;
+	private Integer granularity = 60;
+	
 	
 	@Autowired
-	public BinaryCandlesGather(PacketSender packetSender) {
+	public BinaryCandlesGather(PacketSender packetSender, TicksHistoryHandler ticksHistoryHandler) {
 		this.packetSender = packetSender;
+		this.ticksHistoryHandler = ticksHistoryHandler;
 	}
 	
 	public void getCandles(Stock stock, Long epochFrom, Long epochTo) {
-		Integer count = this.count * 60;
+		Integer step = (count * granularity);
 		
-		while (epochFrom < epochTo) {
-			Packet packet = getPacket(stock, epochFrom, epochFrom + count);
-			epochFrom += count;
+		while (epochFrom <= epochTo) {
+			Long epochFromWithStep = epochFrom + step;
+			Long epochToCalculated =
+					epochFromWithStep <= epochTo
+							? epochFromWithStep
+							: epochTo;
+			Packet packet = getPacket(stock, epochFrom, epochToCalculated);
+			epochFrom = epochFromWithStep + 60;
 			packetSender.send(packet);
 		}
 	}
@@ -37,13 +50,22 @@ public class BinaryCandlesGather {
 		if (stock.getSymbols().stream().filter(symbol -> symbol.getProvider() == StockProvider.BINARY).map(Symbol::getExcluded).findAny().orElse(false))
 			return;
 		
-		Long currentTime = EpochUtil.getCurrentTime();
+		Long currentTime = EpochUtil.getCurrentTimeSeconds();
 		
 		Long start = stock.getStockCandles().isEmpty()
-				? EpochUtil.FirstJanuary2016
-				: stock.getStockCandles().get(stock.getStockCandles().size() - 1).getEpoch();
+				? EpochUtil.getWeekAgoTimeSeconds()
+				: stock.getLastCandle().getEpoch() + granularity;
 		
 		getCandles(stock, start, currentTime);
+	}
+	
+	public Optional<Candle> getLatestCandle(Stock stock) {
+		if (stock.getSymbols().stream().filter(symbol -> symbol.getProvider() == StockProvider.BINARY).map(Symbol::getExcluded).findAny().orElse(false))
+			return Optional.empty();
+		Packet packet = getPacket(stock, 0L, "latest", 1);
+		packet = packetSender.sendAndGet(packet);
+		List<Candle> candles = ticksHistoryHandler.getCandles(packet);
+		return candles.size() > 0 ? Optional.of(candles.get(0)) : Optional.empty();
 	}
 	
 	private Packet getPacket(Stock stock, Long start, Long end) {
@@ -51,6 +73,10 @@ public class BinaryCandlesGather {
 	}
 	
 	private Packet getPacket(Stock stock, Long start, Long end, Integer count) {
+		return getPacket(stock, start, end.toString(), count);
+	}
+	
+	private Packet getPacket(Stock stock, Long start, String end, Integer count) {
 		TicksHistorySend ticksHistory = new TicksHistorySend();
 		ticksHistory.setTicksHistory(stock.getSymbols().stream()
 											 .filter(s ->
@@ -59,11 +85,11 @@ public class BinaryCandlesGather {
 											 .orElse(new Symbol(null, ""))
 											 .getSymbol());
 		ticksHistory.setStart(start);
-		ticksHistory.setEnd(end.toString());
+		ticksHistory.setEnd(end);
 		ticksHistory.setCount(count);
-		ticksHistory.setGranularity(60);
+		ticksHistory.setGranularity(granularity);
 		ticksHistory.setStyle(TicksHistorySend.Style.CANDLES);
 		//ticksHistory.setAdjustStartTime(1);
-		return new Packet(ticksHistory, stock.hashCode());
+		return new Packet(ticksHistory, stock.getId());
 	}
 }
