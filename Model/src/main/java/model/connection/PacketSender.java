442b4d19d2c20137ae1d47a9503c6acbe857837a
WorstCoder <kuriozalnie@gmail.com>
2017-07-12 22:06:22 +0200

    Implemented functionality from decomposed BinaryPacketsService

--- Model/src/main/java/model/connection/PacketSender.java
+++ Model/src/main/java/model/connection/PacketSender.java
@@ -1,21 +1,38 @@
 package model.connection;
 
 import model.binaryAPI.BinaryMessage;
-import model.connection.validation.RequestValidator;
+import model.connection.packetsService.PacketsService;
 import model.data.StockProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import vaer.Vaer;
+
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Component
 public class PacketSender {
 	
 	private final ProviderSender binaryPacketSender;
-	private final RequestValidator requestValidator;
+	
+	private final ExecutorService threadExecutor = Executors.newSingleThreadExecutor();
+	private final AtomicLong sendQueueSize = new AtomicLong(0);
+	
+	private final PacketsService requestValidator;
+	private final PacketsService packetsTimeoutResender;
+	private final PacketsService sentPacketsContainer;
 	
 	@Autowired
-	public PacketSender(ProviderSender binaryPacketSender, RequestValidator requestValidator) {
+	public PacketSender(ProviderSender binaryPacketSender, PacketsService requestValidator, PacketsService packetsTimeoutResender, PacketsService sentPacketsContainer) {
 		this.binaryPacketSender = binaryPacketSender;
 		this.requestValidator = requestValidator;
+		this.packetsTimeoutResender = packetsTimeoutResender;
+		this.sentPacketsContainer = sentPacketsContainer;
+		
+		Vaer.get().group(getClass().getSimpleName()).variable("Messages to send").setVariableGetter(sendQueueSize::get);
 	}
 	
 	public void send(Message message) {
@@ -23,14 +40,16 @@ public class PacketSender {
 	}
 	
 	public void send(Packet packet) {
-		requestValidator.addToSent(packet);
 		StockProvider provider = getProvider(packet);
 		if (provider == null)
 			return;
-		switch (provider) {
-			case BINARY:
-				binaryPacketSender.send(packet);
-		}
+		
+		sendQueueSize.incrementAndGet();
+		threadExecutor.submit(() -> {
+			addToServices(packet);
+			sendByProvider(packet, provider);
+			sendQueueSize.decrementAndGet();
+		});
 	}
 	
 	public Message sendAndGet(Message message) {
@@ -48,6 +67,20 @@ public class PacketSender {
 				System.out.println(e);
 				return null;
 			}
+		}
+	}
+	
+	private void addToServices(Packet packet){
+		requestValidator.addPacket(packet);
+		packetsTimeoutResender.addPacket(packet);
+		sentPacketsContainer.addPacket(packet);
+	}
+	
+	private void sendByProvider(Packet packet, StockProvider provider){
+		switch (provider) {
+			case BINARY:
+				binaryPacketSender.send(packet);
+				break;
 		}
 	}
 	
