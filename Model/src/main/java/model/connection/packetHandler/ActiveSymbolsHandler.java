438df053bede599f085d22db3d17e9673b05cef4
WorstCoder <kuriozalnie@gmail.com>
2017-07-12 22:04:07 +0200

    Decomposed to StocksBinaryDataUpdater

--- Model/src/main/java/model/connection/packetHandler/ActiveSymbolsHandler.java
+++ Model/src/main/java/model/connection/packetHandler/ActiveSymbolsHandler.java
@@ -1,58 +1,43 @@
 package model.connection.packetHandler;
 
 import model.connection.SimpleStream;
-import model.data.BinaryData;
+import model.connection.handleUpdater.StocksBinaryDataUpdater;
+import model.data.*;
 import model.binaryAPI.commands.active_symbols.ActiveSymbolsReceive;
 import model.connection.Packet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class ActiveSymbolsHandler extends SimpleStream<List<BinaryData>> implements PacketHandler {
+@Service
+public class ActiveSymbolsHandler extends SimpleStream<Collection<BinaryData>> implements PacketHandler {
 	
-	private List<BinaryData> binaryData;
+	private final StocksBinaryDataUpdater stocksBinaryDataUpdater;
 	
-	public ActiveSymbolsHandler(){
+	@Autowired
+	public ActiveSymbolsHandler(StocksBinaryDataUpdater stocksBinaryDataUpdater) {
 		super();
+		this.stocksBinaryDataUpdater = stocksBinaryDataUpdater;
 	}
 	
 	@Override
 	public void handle(Packet packet) {
+		List<BinaryData> binaryDataList = getAndAssocBinaryDatas(packet);
+		submit(binaryDataList);
+	}
+	
+	public List<BinaryData> getAndAssocBinaryDatas(Packet packet){
 		ActiveSymbolsReceive activeSymbolsReceive = (ActiveSymbolsReceive) packet.getReceiver();
-		binaryData = activeSymbolsReceive.getActiveSymbols();
-		submit(binaryData);
+		List<BinaryData> binaryDataList = activeSymbolsReceive.getActiveSymbols();
+		stocksBinaryDataUpdater.update(binaryDataList);
+		return binaryDataList;
 	}
 	
 	@Override
 	public Predicate<Packet> getFilter() {
 		return filterByClass(ActiveSymbolsReceive.class);
-	}
-	
-	/*
-	private void addBinaryToStocks(List<BinaryData> binaryStocks) {
-		for (BinaryData binaryData : binaryStocks) {
-			String name = binaryData.getDisplayName();
-			Stock stock = stocks.stream()
-					.filter(s ->
-									s.getName().equals(name))
-					.findFirst()
-					.orElse(new Stock(name));
-			stock.setBinaryData(binaryData);
-			
-			Symbol symbol = new Symbol(StockProvider.BINARY, binaryData.getSymbol());
-			if(!stock.getSymbols().contains(symbol)){
-				stock.addSymbol(symbol);}
-			
-			if(!stocks.contains(stock))
-				stocks.add(stock);
-		}
-	}
-	
-	public List<Stock> getStocks() {
-		return stocks;
-	}
-	*/
-	public List<BinaryData> getBinaryData() {
-		return binaryData;
 	}
 }
