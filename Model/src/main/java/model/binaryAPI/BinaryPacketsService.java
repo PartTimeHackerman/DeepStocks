287e334d7e629d3514e32d4db06ff17470ce5ff3
WorstCoder <kuriozalnie@gmail.com>
2017-07-12 22:02:57 +0200

    Decomposed to PacketSender

--- Model/src/main/java/model/binaryAPI/BinaryPacketsService.java
+++ Model/src/main/java/model/binaryAPI/BinaryPacketsService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import vaer.Vaer;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Vector;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
@@ -19,28 +20,15 @@ public class BinaryPacketsService implements ProviderSender, ProviderReceiver {
 	private final ReceivedPacketsStream packetStream;
 	private final BinaryAPIProvider binaryAPIProvider;
 	
-	private final List<Packet> pendingMessages = new Vector<>();
-	
-	private final ExecutorService threadExecutor = Executors.newSingleThreadExecutor();
-	private final AtomicLong executorQueueSize = new AtomicLong(0);
-	
 	@Autowired
 	public BinaryPacketsService(ReceivedPacketsStream packetStream, BinaryAPIProvider binaryAPIProvider) {
 		this.packetStream = packetStream;
 		this.binaryAPIProvider = binaryAPIProvider;
-		
-		Vaer.get().group(getClass().getSimpleName()).variable("Messages to send").setVariableGetter(executorQueueSize::get);
-		Vaer.get().group(getClass().getSimpleName()).variable("Messages to receive").setVariableGetter(pendingMessages::size);
 	}
 	
 	@Override
 	public void send(Packet packet) {
-		executorQueueSize.incrementAndGet();
-		//MainLogger.log(this).info("!!!!!!!!Sent packet!!!!!!!");
-		threadExecutor.submit(() -> {
-			sendByApi(packet);
-			executorQueueSize.decrementAndGet();
-		});
+		sendByApi(packet);
 	}
 	
 	private void sendByApi(Packet packet) {
@@ -49,37 +37,8 @@ public class BinaryPacketsService implements ProviderSender, ProviderReceiver {
 		binaryAPI.send(packet);
 	}
 	
-	
-	void addToPending(Packet packet) {
-		pendingMessages.add(packet);
-	}
-	
 	@Override
 	public void receive(Packet packet) {
-		//MainLogger.log(this).info("Received: {}", packet.getReceiver().getClass());
-		pendingMessages.remove(packet);
-		if (packet.isWait())
-			notifyMessage(packet);
-		else
 			packetStream.receive(packet);
-	}
-	
-	
-	Packet getFiltered(Predicate<Packet> predicate) {
-		return pendingMessages.stream()
-				.filter(predicate)
-				.findFirst()
-				.orElse(null);
-	}
-	
-	private void notifyMessage(Packet packet) {
-		synchronized (packet) {
-			packet.notify();
-		}
-	}
-	
-	
-	public List<Packet> getPendingMessages() {
-		return pendingMessages;
 	}
 }
