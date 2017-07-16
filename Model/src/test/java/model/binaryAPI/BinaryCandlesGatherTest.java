43474056a79deb29e5b929bd2c2abd2fbf236bcc
WorstCoder <kuriozalnie@gmail.com>
2017-07-16 13:20:05 +0200

    Refactoring

--- Model/src/test/java/model/binaryAPI/BinaryCandlesGatherTest.java
+++ Model/src/test/java/model/binaryAPI/BinaryCandlesGatherTest.java
@@ -1,12 +1,12 @@
 package model.binaryAPI;
 
-import static org.junit.Assert.*;
-
 import model.SpringTest;
 import model.data.Stock;
 import model.data.StockRepo;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertTrue;
 
 @org.springframework.transaction.annotation.Transactional
 public class BinaryCandlesGatherTest extends SpringTest{
@@ -24,7 +24,6 @@ public class BinaryCandlesGatherTest extends SpringTest{
 		binaryCandlesGather.getLatestCandles(stock);
 		Integer afterSize = stock.getStockCandles().size();
 		assertTrue(afterSize - lastSize > 0);
-		
 	}
 	
 }
--- Model/src/test/java/model/connection/proxy/UnrepeatedProxyProviderTest.java
+++ Model/src/test/java/model/connection/proxy/UnrepeatedProxyProviderTest.java
@@ -1,20 +1,7 @@
 package model.connection.proxy;
 
 import model.SpringTest;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import scraper.Proxy;
-
-import static org.junit.Assert.assertNotNull;
 
 public class UnrepeatedProxyProviderTest extends SpringTest {
-	
-	@Autowired
-	private UnrepeatedProxyProvider unrepeatedProxyProvider;
-	
-	@Test
-	public void getProxy() throws Exception {
-		Proxy proxy = unrepeatedProxyProvider.getProxy();
-		assertNotNull(1);
-	}
+
 }
