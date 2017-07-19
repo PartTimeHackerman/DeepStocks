b090e905a878cbed70c5c66e0b9e746485fd0703
WorstCoder <kuriozalnie@gmail.com>
2017-07-19 23:06:11 +0200

    Implemented two useful functions

--- Model/src/main/java/model/dao/StockTradingTimesDAO.java
+++ Model/src/main/java/model/dao/StockTradingTimesDAO.java
@@ -1,9 +1,17 @@
 package model.dao;
 
+import model.data.Stock;
 import model.data.StockTradingTimes;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StockTradingTimesDAO extends CrudRepository<StockTradingTimes, StockTradingTimes.StockTradingTimesPK> {
+	
+	List<StockTradingTimes> findByStock(Stock stock);
+	
+	List<StockTradingTimes> findByStockAndDayEpochIsGreaterThan(Stock stock, Long dayEpoch);
+	
 }
